876fc7ce22b60787cba0f943b0784f9bd1b40dd1
Anshu Patidar <89787727+Anshu-patidar01@users.noreply.github.com>
2025-02-28 14:28:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-// app.use(
-//   cors({
-//     origin: "https://intern-frontend-five.vercel.app/",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://intern-frontend-five.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 connection()
   .then(() =>
